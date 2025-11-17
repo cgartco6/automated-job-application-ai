a5a3256093d012301c919e75e656b3baab3693d8
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-11-17 14:54:21 +0200

    Create init.py

---
+++ src/deep_agents/neural_networks/init.py
@@ -0,0 +1,13 @@
+"""
+Neural Network Models for Deep Learning Tasks
+"""
+
+from .resume_analyzer import ResumeAnalyzer
+from .job_matcher import JobMatcher
+from .interview_simulator import InterviewSimulator
+
+__all__ = [
+    "ResumeAnalyzer",
+    "JobMatcher",
+    "InterviewSimulator"
+]
